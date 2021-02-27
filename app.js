a3b6ddf66e930d81b261dc96bc35144bd1f6066c
bigansh_ <anshpushti2001@gmail.com>
2021-02-27 12:59:15 +0530

    user autneticate, create, & tweet save

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ var express = require("express"),
     mongoose = require("mongoose"),
     expressSanitizer = require("express-sanitizer"),
     twit = require("twit"),
+    passport = require('passport'),
+    Strategy = require('passport-twitter').Strategy,
+    session = require('express-session'),
     app = express();
 
 //TOKENS
 var T = new twit({
-    consumer_key: 'XH1szBHdgbGrB9MCe1Rs461fN',
-    consumer_secret: 'yJM7yYeN3Zsl9nmAPwLr6028pGQHSBN0geRx236nTVBVaP7nXA',
-    access_token: '718377211447930880-WFxtj2PVaaPjyqKIBggUgXeOAw5eqdd',
-    access_token_secret: 'ijMq2HcUoW3DOuxdg01uWrwQXY900oK7vrgomVtHfTGCw',
+    consumer_key: 'byiphhH8THY87ADDYvTv8Upl8',
+    consumer_secret: 'EpV1XVnyLBiZkbUP8E6UzPoGmek4i48jDxmo77HEUKIvArh2tg',
+    // access_token: '718377211447930880-WFxtj2PVaaPjyqKIBggUgXeOAw5eqdd',
+    // access_token_secret: 'ijMq2HcUoW3DOuxdg01uWrwQXY900oK7vrgomVtHfTGCw',
+    access_token: '1350865466340741120-9aHn3REe4EzC1LrQkIqTfjY2Q3DyEX', //tweepsbookapp
+    access_token_secret: '9T4TlmJmDP1ahaEIo5e0U2EEVqVGd6WWlPtSGv3e0ElpB', //tweepsbookapp
     timeout_ms: 60 * 1000,
 })
 
@@ -29,6 +34,9 @@ mongoose.connect("mongodb://localhost/tweepsbookapp", {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'whatever', resave: true, saveUninitialized: true }))
+app.use(passport.initialize());
+app.use(passport.session());
 
 //MONGOOSE MODEL CONFIF/SCHEMA
 var userSchema = mongoose.Schema({
@@ -60,12 +68,61 @@ var tweet = {
     category: String
 };
 
+
+var user = {
+    email: String,
+    profile: String,
+    name: String,
+    id: String
+}
+
 //REQUESTS
-app.get('/', function (req, res) {
-    res.redirect('https://offf.to/tweepsbookapp')
+passport.use(new Strategy({
+    consumerKey: 'byiphhH8THY87ADDYvTv8Upl8',
+    consumerSecret: 'EpV1XVnyLBiZkbUP8E6UzPoGmek4i48jDxmo77HEUKIvArh2tg',
+    userProfileURL: "https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true",
+    callbackURL: '/twitter/return',
+    proxy: true
+}, function (token, tokenSecret, profile, cb) {
+    // console.log(profile);
+    user.id = profile.id;
+    user.email = profile.emails[0].value;
+    user.name = profile.displayName;
+    user.profile = profile.photos[0].value;
+    User.create(user, function (err, user) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(user);
+        }
+    })
+    return cb(null, profile);
+}));
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
 });
 
-var stream = T.stream('statuses/filter', { track: ['@bigansh_ bookmark'] });
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
+app.get('/login/twitter',
+    passport.authenticate('twitter'));
+
+app.get('/twitter/return',
+    passport.authenticate('twitter', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.redirect('/');
+    }
+);
+
+app.get('/', function (req, res) {
+    // res.redirect('https://offf.to/tweepsbookapp')
+    res.render('index')
+});
+
+var stream = T.stream('statuses/filter', { track: ['@tweepsbookapp bookmark'] });
 stream.on('tweet', function (tweet) {
     T.get('statuses/oembed', { id: tweet.in_reply_to_status_id_str }, function (err, data, response) {
         tweet.embed_link = data.html;
@@ -76,7 +133,7 @@ stream.on('tweet', function (tweet) {
         User.find({ id: data.user.id_str }, function (err, user) {
             if (user.length === 0) {
                 params = {
-                    status: 'Hey, you have not registered with us. Hence we are unable to bookmark the tweet you requested. Please register on our website to bookmark better 🤖.',
+                    status: 'Hey, you have not registered with us. Hence we are unable to bookmark the tweet you requested. Please register on our website to bookmark better 🤖. https://offf.to/tweepsbookapp',
                     in_reply_to_status_id: tweet.id_str,
                     auto_populate_reply_metadata: true
                 }
@@ -97,7 +154,7 @@ stream.on('tweet', function (tweet) {
             }
         }).then(function () {
             T.post('statuses/update', params, function (err, data, response) {
-                if (err){
+                if (err) {
                     console.log(err);
                 }
             })
