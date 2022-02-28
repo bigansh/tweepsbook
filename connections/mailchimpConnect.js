6a88313094ae99956a9fb329214b59c433bd2cb5
bigansh <anshpushti2001@gmail.com>
2022-02-28 23:28:04 +0530

    initialize boilerplate

--- connections/mailchimpConnect.js
+++
@@ -1,8 +0,0 @@
-var Mailchimp = require('mailchimp-api-v3'),
-	dotenv = require('dotenv')
-
-dotenv.config()
-
-var mailchimp = new Mailchimp(process.env.MAILCHIMP_API)
-
-module.exports = mailchimp
