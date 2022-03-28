e809a050a9b23e68d32620dfd8c3c12e02b10f66
bigansh <anshpushti2001@gmail.com>
2022-03-29 01:19:28 +0530

    finalize stream & request params

--- backend/bots/twitter/app.js
+++ backend/bots/twitter/app.js
@@ -3,5 +3,3 @@ require('dotenv').config()
 const streamBot = require('./functions/streamBot')
 
 streamBot()
-
-
--- backend/bots/twitter/functions/streamBot.js
+++ backend/bots/twitter/functions/streamBot.js
@@ -1,4 +1,8 @@
-const { ETwitterStreamEvent } = require('twitter-api-v2')
+const {
+	ETwitterStreamEvent,
+	TweetV2SingleStreamResult,
+	TweetStream,
+} = require('twitter-api-v2')
 
 const twtrClient = require('../utils/auth/oauth2.0')
 
@@ -15,12 +19,24 @@ const streamBot = async () => {
 			add: add,
 		})
 
-		const stream = await twtrClient.v2.searchStream()
+		/**
+		 * @type {TweetStream<TweetV2SingleStreamResult>}
+		 */
+		const stream = await twtrClient.v2.getStream('tweets/search/stream', {
+			'tweet.fields': 'referenced_tweets',
+			'tweet.fields': 'entities',
+			expansions: ['author_id', 'referenced_tweets.id'],
+		})
 
 		stream.autoReconnect = true
 		stream.keepAliveTimeoutMs = Infinity
 
-		const tweets = stream.on(ETwitterStreamEvent.Data, (tweets) => tweets)
+		stream.on(ETwitterStreamEvent.Data, ({ data, includes }) => {
+			console.log(data.text, 'text')
+			console.log(data.author_id, 'author')
+			console.log(includes.tweets, 'tweet')
+			console.log(data.entities.hashtags, 'tags')
+		})
 	} catch (error) {
 		console.log(error)
 	}
