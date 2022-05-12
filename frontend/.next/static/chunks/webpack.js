ca5379422f3685b5fa670a30752f2166e4736578
Sarthak Verma <sarthakvdev@gmail.com>
2022-05-12 18:57:21 +0530

    added npm, removed .next

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ec3e546b765db5d"; }
+/******/ 		__webpack_require__.h = function() { return "e2f8b8cb7f2204cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
