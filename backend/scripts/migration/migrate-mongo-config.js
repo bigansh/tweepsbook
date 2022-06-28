9632c2e40be874130e1e03ceeeb86c0668363600
bigansh <anshpushti2001@gmail.com>
2022-06-28 18:05:19 +0530

    add migration scripts

---
+++ backend/scripts/migration/migrate-mongo-config.js
@@ -0,0 +1,16 @@
+const config = {
+	mongodb: {
+		url: process.env.DATABASE_URL,
+		options: {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		},
+	},
+	migrationsDir: 'migrations',
+	changelogCollectionName: 'changelog',
+	migrationFileExtension: '.js',
+	useFileHash: false,
+	moduleSystem: 'esm',
+}
+
+export default config
