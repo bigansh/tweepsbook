d96ab0fd1e171e8fbbb3f7b0168e3b4ba27f4ef6
ayushrungta8 <ayushrungta8@gmail.com>
2022-06-28 18:20:28 +0400

    add import bookmarks and fetch bookmarks fn

--- frontend/pages/app/dashboard.js
+++ frontend/pages/app/dashboard.js
@@ -1,8 +1,52 @@
 import Tags from '../../src/components/Tags'
 import BookmarkCards from '../../src/components/BookmarkCards'
 import SearchBar from '../../src/components/SearchBar'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export default function dashboard() {
+	const [bookmarks, setBookmarks] = useState([])
+	const importBookmarks = async () => {
+		try {
+			const bookmarks = await axios.post(
+				process.env.NEXT_PUBLIC_IMPORT_BOOKMARKS_URL,
+				{},
+				{
+					headers: {
+						Authorization:
+							process.env.NEXT_PUBLIC_TEMP_SESSION_TOKEN,
+					},
+				}
+			)
+			console.log(bookmarks)
+		} catch (err) {
+			throw err
+		}
+	}
+
+	const fetchBookmarks = async () => {
+		try {
+			const bookmarks = await axios.get(
+				process.env.NEXT_PUBLIC_FETCH_BOOKMARKS_URL,
+				{
+					headers: {
+						Authorization:
+							process.env.NEXT_PUBLIC_TEMP_SESSION_TOKEN,
+					},
+				}
+			)
+			console.log(bookmarks)
+			setBookmarks(bookmarks.data)
+		} catch (err) {
+			throw err
+		}
+	}
+
+	useEffect(() => {
+		importBookmarks()
+		fetchBookmarks()
+	}, [])
+
 	return (
 		<div className='min-h-screen overflow-hidden'>
 			<div className='flex items-center justify-center h-24 border border-black'>
@@ -15,7 +59,7 @@ export default function dashboard() {
 				</div>
 
 				<div className='w-5/6'>
-					<BookmarkCards />
+					<BookmarkCards bookmarks={bookmarks} />
 				</div>
 			</div>
 		</div>
--- frontend/src/components/BookmarkCards.js
+++ frontend/src/components/BookmarkCards.js
@@ -1,29 +1,17 @@
 import React from 'react'
 
-const bookmarkCards = () => {
-  return (
-    <div className="flex flex-wrap justify-evenly  p-1 m-3 h-full w-full">
-        <div className='flex rounded-md flex-col items-center justify-center border w-1/4 h-60 p-1 m-2 '>
-            <h1>bookmarkCards</h1>
-        </div>
-        <div className='flex rounded-md flex-col items-center justify-center border w-1/4 h-60 p-1 m-2 '>
-            <h1>bookmarkCards</h1>
-        </div>
-        <div className='flex rounded-md flex-col items-center justify-center border w-1/4 h-60 p-1 m-2 '>
-            <h1>bookmarkCards</h1>
-        </div>
-        <div className='flex rounded-md flex-col items-center justify-center border w-1/4 h-60 p-1 m-2 '>
-            <h1>bookmarkCards</h1>
-        </div>
-        <div className='flex rounded-md flex-col items-center justify-center border w-1/4 h-60 p-1 m-2 '>
-            <h1>bookmarkCards</h1>
-        </div>
-        <div className='flex rounded-md flex-col items-center justify-center border w-1/4 h-60 p-1 m-2 '>
-            <h1>bookmarkCards</h1>
-        </div>
-        
-    </div>
-  )
+const bookmarkCards = ({ bookmarks }) => {
+	return (
+		<div className='flex flex-wrap justify-evenly  p-1 m-3 h-full w-full'>
+			{bookmarks?.map((bookmark) => {
+				return (
+					<div className='flex rounded-md flex-col items-center justify-center border w-1/4 h-60 p-1 m-2 '>
+						<h1>{bookmark?.content}</h1>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default bookmarkCards
