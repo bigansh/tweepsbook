a800fecc73fa45564ee25716665f5396258085fb
Nawed Ali <nawed2611@gmail.com>
2022-07-04 02:25:49 +0530

    Update dashboard

--- frontend/src/components/BookmarkCards.js
+++ frontend/src/components/BookmarkCards.js
@@ -1,15 +1,91 @@
 import React from 'react'
+import { BiDotsVerticalRounded } from 'react-icons/bi';
+import { BiTrashAlt } from 'react-icons/bi';
+import { BiArchiveIn } from 'react-icons/bi';
+import { BsTwitter } from "react-icons/bs";
 
 const bookmarkCards = ({ bookmarks }) => {
 	return (
-		<div className='flex flex-wrap justify-evenly  p-1 m-3 h-full w-full'>
-			{bookmarks?.map((bookmark) => {
+		<div className='flex flex-wrap m-3 justify-start  w-full'>
+			{/* { !bookmarks && bookmarks.map((bookmark) => {
 				return (
 					<div className='flex rounded-md flex-col items-center justify-center border w-1/4 h-60 p-1 m-2 '>
 						<h1>{bookmark?.content}</h1>
 					</div>
 				)
-			})}
+			})} */}
+			<div className='flex flex-col rounded-md border w-96 h-40 p-1 m-2 '>
+				<div className='w-full flex items-center'>
+					<img src="https://pbs.twimg.com/profile_images/1541300431623598080/QNrH1wh1_400x400.jpg" className='w-10 h-10 rounded-full m-1 p-1' />
+					<span className='mx-1'>Nawed Ali</span>
+					<span className='mx-1 font-thin text-xs'>@nawed2611 • 27 June 2022</span>
+					<div className='flex items-center p-2 justify-end'>
+					<BsTwitter className='mx-3' />
+					</div>
+				</div>
+				<p className='m-1 p-1'>
+					lorem ipsum dolor sit amet consectetur adipisicing elit.
+				</p>
+				<div className='flex items-center opacity-50 p-2 justify-end'>
+					<BiArchiveIn className='mx-3 w-5 h-5' />
+					<BiTrashAlt className='mx-3 w-5 h-5' />
+					<BiDotsVerticalRounded className='mx-3 w-5 h-5' />
+				</div>
+			</div>
+			<div className='flex flex-col rounded-md border w-96 h-40 p-1 m-2 '>
+				<div className='w-full flex items-center'>
+					<img src="https://pbs.twimg.com/profile_images/1541300431623598080/QNrH1wh1_400x400.jpg" className='w-10 h-10 rounded-full m-1 p-1' />
+					<span className='mx-1'>Nawed Ali</span>
+					<span className='mx-1 font-thin text-xs'>@nawed2611 • 27 June 2022</span>
+					<div className='flex items-center p-2 justify-end'>
+					<BsTwitter className='mx-3' />
+					</div>
+				</div>
+				<p className='m-1 p-1'>
+					lorem ipsum dolor sit amet consectetur adipisicing elit.
+				</p>
+				<div className='flex items-center opacity-50 p-2 justify-end'>
+					<BiArchiveIn className='mx-3 w-5 h-5' />
+					<BiTrashAlt className='mx-3 w-5 h-5' />
+					<BiDotsVerticalRounded className='mx-3 w-5 h-5' />
+				</div>
+			</div>
+			<div className='flex flex-col rounded-md border w-96 h-40 p-1 m-2 '>
+				<div className='w-full flex items-center'>
+					<img src="https://pbs.twimg.com/profile_images/1541300431623598080/QNrH1wh1_400x400.jpg" className='w-10 h-10 rounded-full m-1 p-1' />
+					<span className='mx-1'>Nawed Ali</span>
+					<span className='mx-1 font-thin text-xs'>@nawed2611 • 27 June 2022</span>
+					<div className='flex items-center p-2 justify-end'>
+					<BsTwitter className='mx-3' />
+					</div>
+				</div>
+				<p className='m-1 p-1'>
+					lorem ipsum dolor sit amet consectetur adipisicing elit.
+				</p>
+				<div className='flex items-center opacity-50 p-2 justify-end'>
+					<BiArchiveIn className='mx-3 w-5 h-5' />
+					<BiTrashAlt className='mx-3 w-5 h-5' />
+					<BiDotsVerticalRounded className='mx-3 w-5 h-5' />
+				</div>
+			</div>
+			<div className='flex flex-col rounded-md border w-96 h-40 p-1 m-2 '>
+				<div className='w-full flex items-center'>
+					<img src="https://pbs.twimg.com/profile_images/1541300431623598080/QNrH1wh1_400x400.jpg" className='w-10 h-10 rounded-full m-1 p-1' />
+					<span className='mx-1'>Nawed Ali</span>
+					<span className='mx-1 font-thin text-xs'>@nawed2611 • 27 June 2022</span>
+					<div className='flex items-center p-2 justify-end'>
+					<BsTwitter className='mx-3' />
+					</div>
+				</div>
+				<p className='m-1 p-1'>
+					lorem ipsum dolor sit amet consectetur adipisicing elit.
+				</p>
+				<div className='flex items-center opacity-50 p-2 justify-end'>
+					<BiArchiveIn className='mx-3 w-5 h-5' />
+					<BiTrashAlt className='mx-3 w-5 h-5' />
+					<BiDotsVerticalRounded className='mx-3 w-5 h-5' />
+				</div>
+			</div>
 		</div>
 	)
 }
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const searchBar = () => {
     return (
-
-        <input type="text" className="m-1 p-2 rounded-md border w-1/2" placeholder="Search here for your bookmarks..." />
-
+        <div className='p-1 flex items-center justify-between w-1/2 h-1/2'>
+            <input type="text" className="focus:outline-none m-1 p-2 pl-8 font-light border rounded-full w-full" placeholder="Which bookmark are you looking for?" />
+            <button className='relative right-10'><AiOutlineSearch /></button>
+        </div>
     )
 }
 
