26bc3ef7ef6127d748eec04123cfa231df17404e
Nawed Ali <nawed2611@gmail.com>
2022-07-05 17:30:02 +0530

    Add Import and Archive buttons

--- frontend/src/components/Tags.js
+++ frontend/src/components/Tags.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react'
+import { BiArchiveIn } from 'react-icons/bi'
+import { BiImport } from 'react-icons/bi'
 
 const tags = () => {
 
-  const [activeTag, setActiveTag] = useState(false);
+  const [activeTag, setActiveTag] = useState(true);
 
   const handleClick = () => {
     setActiveTag(!activeTag);
   }
 
   return (
-    <div className="flex flex-col items-start h-full w-full mt-2 text-white">
+    <div className="flex flex-col items-start h-screen w-full mt-2 text-white">
       <h1 className='text-xl mb-10 font-bold pl-5'>TAGS</h1>
-      <div className='flex text-md w-full flex-col items-center'>
-        <button onClick={handleClick} className={"m-1 px-4 " + ( activeTag ? "bg-sh-gray" : "") }>#nft</button>
+      <div className='flex text-md justify-between h-full w-full flex-col items-center'>
+        <div className='flex flex-col'>
+          <button onClick={handleClick} className={"m-1 px-8 py-1 " + (activeTag ? "bg-hovertagColor" : "")}>#all</button>
+          <button onClick={handleClick} className={"m-1 px-8 py-1 " + (activeTag ? "bg-hovertagColor" : "")}>#all</button>
+          <button onClick={handleClick} className={"m-1 px-8 py-1 " + (activeTag ? "bg-hovertagColor" : "")}>#all</button>
+        </div>
+
+        <div className='flex text-md border-t w-full flex-col items-center'>
+          <button className='flex items-center px-4 h-8 m-2'><BiImport className='mr-2' />Import</button>
+          <button className='flex items-center px-4 m-2 h-8'><BiArchiveIn className='mr-2' />Archive</button>
+        </div>
       </div>
+
     </div>
   )
 }
