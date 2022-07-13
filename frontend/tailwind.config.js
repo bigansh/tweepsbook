920c04696750bfa74b9f2e3c4d96c163427ee02f
Sarthak Verma <sarthakvdev@gmail.com>
2022-07-13 20:19:04 +0530

    tag navbar done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,24 +4,24 @@ module.exports = {
 		'./src/components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		colors: {
-			'dark-blue': '#004965',
-			'mid-blue': '#29C3FE',
-			'light-blue': '#ADE8FF',
-			'light-blue-text': 'rgba(0, 73, 100, 0.6)',
-			'sh-gray': '#70878F',
-			hovertagColor: '#003447',
-			'lg-gray': '#F2F2F2',
-			white: '#ffffff',
-			'lg-orange': '#FF4949',
-		},
-		fontFamily: {
-			serif: ['ui-serif', 'Merriweather', 'serif'],
-		},
 		minHeight: {
 			eighty: '78vh',
 		},
 		extend: {
+			colors: {
+				'dark-blue': '#004965',
+				'mid-blue': '#29C3FE',
+				'light-blue': '#ADE8FF',
+				'light-blue-text': 'rgba(0, 73, 100, 0.6)',
+				'sh-gray': '#70878F',
+				hovertagColor: '#003447',
+				'lg-gray': '#F2F2F2',
+				white: '#ffffff',
+				'lg-orange': '#FF4949',
+			},
+			fontFamily: {
+				serif: ['ui-serif', 'Merriweather', 'serif'],
+			},
 			keyframes: {
 				scale: {
 					'0%': {
