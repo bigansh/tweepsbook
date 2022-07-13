b338d9fba194c69e5ca8b48ae59d83325065d202
ayushrungta8 <ayushrungta8@gmail.com>
2022-07-13 09:28:32 +0530

    fixed sidebar

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -9,7 +9,21 @@ const MyApp = ({ Component, pageProps }) => {
 	return (
 		<BookmarksProvider>
 			<UserProvider>
-				<Head></Head>
+				<Head>
+					<link
+						rel='preconnect'
+						href='https://fonts.googleapis.com'
+					/>
+					<link
+						rel='preconnect'
+						href='https://fonts.gstatic.com'
+						crossorigin
+					/>
+					<link
+						href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap'
+						rel='stylesheet'
+					/>
+				</Head>
 
 				<Component {...pageProps} />
 			</UserProvider>
