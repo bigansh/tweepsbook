d161f885a602c66f48ebf8229536a862d0b60fa5
Sarthak Verma <sarthakvdev@gmail.com>
2022-07-13 22:23:48 +0530

    tagpill design done

--- frontend/src/components/TagPill.js
+++ frontend/src/components/TagPill.js
@@ -85,12 +85,12 @@ const TagPill = ({ bookmark }) => {
 				return (
 					<div className='mx-1' key={tag._id}>
 						{!tag.showEdit && (
-							<div className='border-solid border flex items-center border-dark-blue px-2 py-1 rounded-full my-1 text-sm text-[#004965] font-semibold'>
+							<div className='border flex items-center border-dark-blue text-sm font-normal px-2 py-1 rounded-full my-1 text-dark-blue'>
 								<span onClick={() => editTag(tag)}>
 									#{tag.tag}
 								</span>
 								<IoIosClose
-									className='ml-1 w-5 h-5 cursor-pointer'
+									className='w-5 h-5 cursor-pointer rounded-full hover:bg-dark-blue opacity-50 hover:bg-opacity-5 hover:opacity-100 transition-all duration-100'
 									onClick={() => deleteTag(tag)}
 								/>
 							</div>
