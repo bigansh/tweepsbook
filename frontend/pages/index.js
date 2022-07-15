76907bfab04169b4794c65a6bceba021be4f5931
Nawed Ali <nawed2611@gmail.com>
2022-07-15 14:12:59 +0530

    Add favicon

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,20 +13,20 @@ const Home = () => {
 				<meta property="og:description" content="What Twitter could not do with Twitter Blue, we did it for free. TweepsBook is a better bookmarking tool that allows you to import, organize, take, & share notes on all your “gyan-worthy” bookmarks." />
 				<meta property="og:image" content="https://i.postimg.cc/Qd2WknqK/Message-Header.png" />
 				<Script async src="https://www.googletagmanager.com/gtag/js?id=G-J0QPJQF85V"></Script>
-				<Script>
+				{/* <Script>
 					window.dataLayer = window.dataLayer || [];
 					function gtag(){dataLayer.push(arguments)}
 					gtag('js', new Date());
 
 					gtag('config', 'G-J0QPJQF85V')
-				</Script>
-				<Script>
-					{!function() {var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked = !0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="1t7Yw2EonU8mBWTA6X0FQNphh5fhN6Ah";;analytics.SNIPPET_VERSION="4.15.3";
+				</Script> */}
+				{/* <Script>
+					!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked = !0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="1t7Yw2EonU8mBWTA6X0FQNphh5fhN6Ah";;analytics.SNIPPET_VERSION="4.15.3";
 					analytics.load("1t7Yw2EonU8mBWTA6X0FQNphh5fhN6Ah");
-					analytics.page()}}}();
-				</Script>
+					analytics.page();}}()
+				</Script> */}
 			</Head>
-			
+
 			<Hero />
 		</>
 	)
