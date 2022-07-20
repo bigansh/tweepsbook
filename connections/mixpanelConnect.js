dbb935c06532fb897185394c8a5d026c0f9a56d8
bigansh <anshpushti2001@gmail.com>
2022-07-20 15:53:54 +0530

    delete content

--- connections/mixpanelConnect.js
+++
@@ -1,8 +0,0 @@
-var Mixpanel = require('mixpanel'),
-	dotenv = require('dotenv')
-
-dotenv.config()
-
-var mixpanel = Mixpanel.init(process.env.MIXPANEL_TOKEN)
-
-module.exports = mixpanel
--- models/tweets.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-
-var tweetsSchema = mongoose.Schema({
-	status: String,
-	tag: String,
-	id: String,
-})
-
-module.exports = mongoose.model('Tweet', tweetsSchema)
